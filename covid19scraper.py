1fb7b8595f7f633a5f99c974b2d445f08ff0fd6f
Nick <nickthuazn@gmail.com>
2020-07-06 19:26:30 -0700

    adding driver files

---
+++ covid19scraper.py
@@ -0,0 +1,33 @@
+import requests,datetime
+from bs4 import BeautifulSoup
+
+def scrapeGlobalCase ():
+    try:
+        url = "https://www.worldometers.info/coronavirus/usa/california/"
+        req = requests.get(url)
+        bsObj = BeautifulSoup(req.text, "html.parser")
+        bigData = bsObj.find_all("div",class_ = "maincounter-number")
+
+        table = bsObj.find('table', id="usa_table_countries_today")
+        rows = table.find_all('tr')
+
+        data = []
+        for row in rows:
+            cols = row.find_all('td')
+            data.append(cols)
+
+        print(data[3][1].contents[0].replace(',', ''))
+
+        i = 2
+        total = 0
+        while i < len(data) - 5:
+            total += int(data[i][1].contents[0].replace(',', ''))
+            i+=1
+        TimeNow = datetime.datetime.now()
+        return {
+            'date': str(TimeNow),
+            'total': str(total)
+        }
+        
+     
+    except Exception as e: print(e)
